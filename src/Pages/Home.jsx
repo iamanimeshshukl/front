029f5006bec659cd2eccb76c703f8d2d270f5f5b
iamanimeshshukl <animeshshuklafoundation@gmail.com>
2025-04-07 18:07:18 +0530

    new

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 //import Video from '../components/Video.jsx'
 import Tracker from "../components/Tracker.jsx"
 //import Aboutus from '../components/Aboutus.jsx'
-import Ai from "../components/Ai.jsx"
+//import Ai from "../components/Ai.jsx"
 import Productdeatils from '../components/Productdetails.jsx'
 import Hero from '../components/Hero'
 import Mentor from "../components/Mentor.jsx"
@@ -11,8 +11,7 @@ import Gallery from '../components/Gallery.jsx'
 import Newsletter from '../components/Newsletter.jsx'
 import Free from '../components/Free.jsx'
 import Article from '../components/Article.jsx'
-//import Quiz from '../components/Quiz'
-``
+import Quiz from '../components/Quiz'
 //import { Helmet } from 'react-helmet-async';
 const Home = () => {
   return (
@@ -30,7 +29,7 @@ const Home = () => {
       <Free/>
       <Newsletter/>
       <Faq/>
-      <Ai />
+      <Quiz />
       
       
      
